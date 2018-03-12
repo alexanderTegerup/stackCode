3c1029e779f241f9234061e28d4ae18347477b53
ategerup <alexander.tegerup@gmail.com>
2018-03-12 18:47:06 +0100

    Bug fixed. Read more about the bug and improve the code.

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main()
 
     while(runProgram){
         printf("1: Add an element to the stack\n 2: Delete an element on the stack\n 3: Print out all elements on the stack\n 4: Print out a single element\n 5: Exit\n");
-        scanf("%d", &option);
+        scanf(" %d", &option);
 
             switch(option){
                 case 1 : push();
@@ -44,7 +44,7 @@ void push(){
     }else{
         char inputElm;
         printf("Please add an element to the stack: \n\n");
-        scanf("%c",&inputElm);
+        scanf(" %c",&inputElm);
         printf("Input element is: %c\n",inputElm);
         top++;
         stack[top] = inputElm;
@@ -65,7 +65,7 @@ void printElm(){
     if(top>=0){
         int elm;
         printf("Which element would you like to print out? Choose from 0 to %d\n",top);
-        scanf("%d",&elm);
+        scanf(" %d",&elm);
         if(elm)
         printf("Element %d: %c\n",elm,stack[elm]);
     } else{
