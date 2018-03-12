b0daad995102ac428a49733f948a27b5f6b6d4ad
ategerup <alexander.tegerup@gmail.com>
2018-03-12 18:14:17 +0100

    Very strange bug if I try to add element to the stack

--- main.c
+++ main.c
@@ -6,17 +6,22 @@ int top = -1; // The top of the stack, initialized for an empty stack.
 char stack[SIZESTACK];
 
 void push(){
-    char inputElm;
-    printf("Please add an element to the stack: \n");
-    scanf("%c",&inputElm);
-    top++;
-    stack[top] = inputElm;
+    if(top+1 >= SIZESTACK){
+        printf("Can't add another element, because the stack is full\n");
+    }else{
+        char inputElm;
+        printf("Please add an element to the stack: \n\n");
+        scanf("%c",&inputElm);
+        printf("Input element is: %c\n",inputElm);
+        top++;
+        stack[top] = inputElm;
+    }
 }
 
 void printAll(){
     if(top>=0){
-        for(int i=0; i<top; i++){
-            printf("Element %d: %c\n",i,*stack);
+        for(int i=0; i<=top; i++){
+            printf("Element %d: %c\n",i,stack[i]);
         }
     }else{
         printf("The stack is empty\n");
@@ -25,38 +30,47 @@ void printAll(){
 
 void printElm(){
     if(top>=0){
-        printf("Which element would you like to print out? Choose from to %d\n",top);
+        int elm;
+        printf("Which element would you like to print out? Choose from 0 to %d\n",top);
+        scanf("%d",&elm);
+        if(elm)
+        printf("Element %d: %c",elm,stack[elm]);
     } else{
         printf("The stack is empty\n");
     }
 }
 
+void pop(){
+    if(top>=0){
+        top--;
+    }else{
+        printf("There are no elements to remove.\n");
+    }
+}
 
 int main()
 {
     int option;
-    int exit = 1;
-    push();
+    int runProgram = 1;
 
-    while(exit){
+    while(runProgram){
         printf("1: Add an element to the stack\n 2: Delete an element on the stack\n 3: Print out all elements on the stack\n 4: Print out a single element\n 5: Exit\n");
         scanf("%d", &option);
 
             switch(option){
                 case 1 : push();
                     break;
-                case 2 : printf("Test\n");
+                case 2 : pop();
                     break;
                 case 3 : printAll();
                     break;
                 case 4 : printElm();
                     break;
-                case 5 : exit=0;
+                case 5 : runProgram=0;
                     break;
                 default : printf("You have to enter an integer between 1-5.\n");
             }
     }
-
 
     return 0;
 }
